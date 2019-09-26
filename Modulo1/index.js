94dc8c5612fdef0f1ab06f4dc41e3e0aad047841
Victor Piassi <victor.santos@kumulus.com.br>
2019-09-26 16:57:27 -0300

    Modulo 1 - MiddleWare

--- Modulo1/index.js
+++ Modulo1/index.js
@@ -1,8 +1,23 @@
-const http = require("http");
+const express = require("express");
 
-http
-  .createServer((req, res) => {
-    console.log(req);
-    return res.end("Hello Word!");
-  })
-  .listen(3000);
+const app = express();
+
+const logMiddleWare = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+
+  return next();
+};
+
+app.use(logMiddleWare);
+
+app.get("/", (req, res) => {
+  return res.send(`Bem Vindo(a), ${req.query.name}`);
+});
+
+app.get("/nome/:name", (req, res) => {
+  return res.send(`Bem vindo(a), ${req.params.name}`);
+});
+
+app.listen(3000);
